8028808429e0e4a333bb9c2086fd6d46f2da2863
paola samaniego <115908451+paolasama@users.noreply.github.com>
2025-07-23 18:12:40 -0700

    first commit

---
+++ product/src/main/java/com/paolasama/product/entities/product.java
@@ -0,0 +1,21 @@
+package com.paolasama.product.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
